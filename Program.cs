e164803bf8fe550293e166100e06412769a33663
Rafael Sitompul <141505074+RAFAELSITOMPUL@users.noreply.github.com>
2025-03-09 20:17:59 +0700

    Add files via upload
    tugas aritmatika

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan angka pertama: ");
+        double angka1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Masukkan angka kedua: ");
+        double angka2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\nHasil Operasi Aritmatika:");
+        Console.WriteLine($"{angka1} + {angka2} = {angka1 + angka2}");
+        Console.WriteLine($"{angka1} - {angka2} = {angka1 - angka2}");
+        Console.WriteLine($"{angka1} * {angka2} = {angka1 * angka2}");
+
+        if (angka2 != 0)
+        {
+            Console.WriteLine($"{angka1} / {angka2} = {angka1 / angka2}");
+        }
+        else
+        {
+            Console.WriteLine("Pembagian tidak dapat dilakukan (pembagi nol).");
+        }
+    }
+}
